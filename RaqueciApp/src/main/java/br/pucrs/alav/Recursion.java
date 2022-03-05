79925cb581b4a6670f860e4b0e8808f7a65c8271
Arthur Zanella Lovato <arthur.zanella.lovato.1998@gmail.com>
2022-03-05 19:37:04 -0300

    Adicionado exercícios 3, 4 e 8

--- RaqueciApp/src/main/java/br/pucrs/alav/Recursion.java
+++ RaqueciApp/src/main/java/br/pucrs/alav/Recursion.java
@@ -4,7 +4,13 @@ public class Recursion {
     public static void main (String args []){
         System.out.println("Resposta 1: " + mult(4,6));
         System.out.println("Resposta 2: " + soma(3,2));
-        System.out.println("Resposta 2: " + divisao(5));
+        System.out.println("Resposta 3: " + divisao(5));
+        String s1 = "Papagaio".replaceAll("\\s", "").toUpperCase();
+        System.out.println("Resposta 4: " + Inverse(s1));
+        String s2 = "Luz Azul".replaceAll("\\s", "").toUpperCase();
+        System.out.println("Resposta 8: " + Palindrome(s2));
+        
+        
     }
 	
     private static int mult(int num1, int num2){
@@ -25,7 +31,35 @@ public class Recursion {
         }
     }
 
-    private static int divisao(int num1, int num2){
-
+    private static double divisao (double num){
+    	if(num == 1){
+            return num;
+        }else{
+        	double result = 1 / num;
+            return result + divisao(num-1);
+        }
+    }
+    
+    public static String Inverse(String word)
+    {   
+    	if(word.length() == 0 || word.length() == 1){
+            return word;
+        }else{
+        	char w1 = word.charAt(0);
+        	char w2 = word.charAt(word.length()-1);
+    	
+        	return w2 + Inverse(word.substring(1, word.length()-1)) + w1;
+        }
+    }
+    
+    public static String Palindrome(String word)
+    {   
+        if(word.length() == 0 || word.length() == 1) {
+            return "É Palindromo"; 
+        }
+        if(word.charAt(0) == word.charAt(word.length()-1)) {
+        	return Palindrome(word.substring(1, word.length()-1));
+        }
+        return "Não é Palindromo";
     }
 }
