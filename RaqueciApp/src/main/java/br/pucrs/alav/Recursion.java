8fa8f4d8dcb7808139213f96626afc71c054ae96
Christian Goulart <christian.goulart@edu.pucrs.br>
2022-03-06 19:13:36 -0300

    Feitas algumas dos outros exercicios

--- RaqueciApp/src/main/java/br/pucrs/alav/Recursion.java
+++ RaqueciApp/src/main/java/br/pucrs/alav/Recursion.java
@@ -1,4 +1,5 @@
 package br.pucrs.alav;
+import java.util.Arrays;
 
 public class Recursion {
     public static void main (String args []){
@@ -7,10 +8,12 @@ public class Recursion {
         System.out.println("Resposta 3: " + divisao(5));
         String s1 = "Papagaio".replaceAll("\\s", "").toUpperCase();
         System.out.println("Resposta 4: " + Inverse(s1));
+        System.out.println("Resposta 5: " + sequencia1(5));
+        //System.out.println("Resposta 6: " + sequenciaAckerman(7,4)); Erro estranho
+        System.out.println("Resposta 7: " + somaDeVetor(new int[]{5,5,6,9,8,2,3}));
         String s2 = "Luz Azul".replaceAll("\\s", "").toUpperCase();
         System.out.println("Resposta 8: " + Palindrome(s2));
-        
-        
+    
     }
 	
     private static int mult(int num1, int num2){
@@ -55,11 +58,43 @@ public class Recursion {
     public static String Palindrome(String word)
     {   
         if(word.length() == 0 || word.length() == 1) {
-            return "É Palindromo"; 
+            return "ï¿½ Palindromo"; 
         }
         if(word.charAt(0) == word.charAt(word.length()-1)) {
         	return Palindrome(word.substring(1, word.length()-1));
         }
-        return "Não é Palindromo";
+        return "Nï¿½o ï¿½ Palindromo";
+    }
+
+    public static int sequencia1(int num){
+        if(num == 1){
+            return 1;
+        }
+        if(num == 2){
+            return 2;
+        }else{
+            return (2*sequencia1(num-1)) + (3*sequencia1(num-2));
+        }
+    }
+
+    public static int sequenciaAckerman(int m, int n){
+        if(m == 0){
+            return n+1;
+        }
+        if(n == 0){
+            return sequenciaAckerman(m-1,1);
+        }
+        else{
+            return sequenciaAckerman(m - 1, sequenciaAckerman(m, n - 1));
+        }
+    }
+
+    public static int somaDeVetor(int[] nums){
+        if(nums.length == 1){
+            return nums[0];
+        }else{
+            int[] newNums = Arrays.copyOfRange(nums, 1, nums.length);
+            return nums[0] + somaDeVetor(newNums);
+        }
     }
 }
